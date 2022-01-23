bc576d62d11180e50d415297f7daf09e610380d5
Brooke Rhodes <brooke@gril.me>
2022-01-23 19:22:35 +0000

    Add meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,26 @@ class MyDocument extends Document {
             <Html>
                 <Head>
                     <link href="https://rsms.me/inter/inter.css" rel="stylesheet" />
+
+                    {/* Primary Meta Tags */}
+                    {/* eslint-disable-next-line @next/next/no-title-in-document-head */}
+                    <title>Merlin</title>
+                    <meta name="title" content="Merlin" />
+                    <meta name="description" content="Merlin is a Roblox moderation panel, built for scale." />
+
+                    {/* Open Graph / Facebook */}
+                    <meta property="og:type" content="website" />
+                    <meta property="og:url" content="https://merlinrbx.com/" />
+                    <meta property="og:title" content="Merlin" />
+                    <meta property="og:description" content="Merlin is a Roblox moderation panel, built for scale." />
+
+                    {/* Twitter */}
+                    <meta property="twitter:url" content="https://merlinrbx.com/" />
+                    <meta property="twitter:title" content="Merlin" />
+                    <meta
+                        property="twitter:description"
+                        content="Merlin is a Roblox moderation panel, built for scale."
+                    />
                 </Head>
                 <body>
                     <Main />
