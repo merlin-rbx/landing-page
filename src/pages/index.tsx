576cfa4116eb6ca74d0de5d8315a51e443a7dfac
Brooke Rhodes <brooke@gril.me>
2022-01-23 17:46:42 +0000

    Reorganize and fix tailwind

---
+++ src/pages/index.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+    return <div className="w-screen h-screen overflow-hidden" />;
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,17 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
     content: [
-        "./pages/**/*.{js,ts,jsx,tsx}",
-        "./components/**/*.{js,ts,jsx,tsx}",
+        "./src/pages/**/*.{js,ts,jsx,tsx}",
+        "./src/components/**/*.{js,ts,jsx,tsx}",
+        "./src/layouts/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+                sans: ["Inter var", ...defaultTheme.fontFamily.sans],
             },
         },
     },
     plugins: [],
-}
+};
